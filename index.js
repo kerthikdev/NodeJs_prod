e2938e0adacf33005eeebe8bd72555423332d8ac
kerthikdev <kerthikdev@gmail.com>
2025-04-02 10:07:31 +0530

    ci cd

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route for the homepage
+app.get('/', (req, res) => {
+  res.send('Hello, Node.js Application !');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
